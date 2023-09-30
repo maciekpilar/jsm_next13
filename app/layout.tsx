f01f0adef48724612a3e264f35afab1f0c1e5815
Maciej Pilarski <maciekpilar12@gmail.com>
2023-09-30 23:19:41 +0200

    Fix skeleton color, improve metadata, change favicon

--- app/layout.tsx
+++ app/layout.tsx
@@ -4,6 +4,15 @@ import type { Metadata } from "next";
 export const metadata: Metadata = {
   title: "JS Mastery",
   description: "JSM Platform with Next 13",
+  other: {
+    "theme-color": "#0d1117",
+    "color-sheme": "dark only",
+    "twitter:image": "https://i.ibb.co/d6TXxB2/homepage-thumbnail.jpg",
+    "twitter:card": "summary_large_image",
+    "og:url": "jsmastery.pro",
+    "og:image": "https://i.ibb.co/d6TXxB2/homepage-thumbnail.jpg",
+    "og:type": "website",
+  },
 };
 
 export default function RootLayout({
